7fcf19b3480e12024bfb7f13d533e77fdda9c1ab
Sainna <10864365+Sainna@users.noreply.github.com>
2022-01-05 15:57:13 +0100

    Fix sample namespace usage

--- Samples~/CubeKnock/MouseClickImpact.cs
+++ Samples~/CubeKnock/MouseClickImpact.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sainna.Onomatopoeia;
 
 public class MouseClickImpact : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown (0)){ 
-            Debug.Log("1");
+        if (Input.GetMouseButtonDown (0)){
             RaycastHit hit; 
             Camera cam = GameObject.FindGameObjectWithTag("MainCamera")?.GetComponent<Camera>();
             Ray ray = cam.ScreenPointToRay(Input.mousePosition); 
             if ( Physics.Raycast (ray,out hit,100.0f)) {
-                Debug.Log("2");
                 ColliderImpact csound = hit.rigidbody?.gameObject.GetComponentInChildren<ColliderImpact>();
                 csound?.SpawnAnimation(hit.point, hit.normal);
             }
